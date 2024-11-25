999111debccc569d989a78ba99846d9ed20545d6
Ali Muhammad <145827451+alicode56@users.noreply.github.com>
2024-11-25 23:13:04 +0500

    continue

---
+++ 01_bsics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId= 1234567
+let accountEmail = "ali@12gmail.com"
+var accountPassword = "12321"
+accountCity = "karachi"
+
+//const accountId=1
+
+let accountEmail = "ali@01gmail.com"
+var accountPassword = "123211"
+accountCity = "sukur"
+
+console.log('accountId');
+
+console.log([accountId,accountId,accountPassword])
